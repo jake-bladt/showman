50394d0663b03e01bc8079b71e94c0c963eea9d8
Jake Bladt <jake@jakebladt.com>
2016-12-28 21:45:46 -0500

    extract db url

--- api/src/api.js
+++ api/src/api.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const router = express.Router();
+
 const port = process.env.PORT || 8088;
+const dburl = process.env.DBURL || secrets.DBUrl;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
