a14299dbd6ed3ebda560659acc21699e26feebad
Jake Bladt <jake@jakebladt.com>
2016-12-28 21:28:21 -0500

    use express routing

--- api/src/api.js
+++ api/src/api.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const router = express.Router();
+const port = process.env.PORT || 8088;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 8088;
