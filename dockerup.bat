2e77d87442344ef9fbdc979029c8f1816d48d955
Jake Bladt <jake@jakebladt.com>
2016-12-28 20:08:58 -0500

    -extra docker artifacts

--- dockerup.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-SET DOCKER_TLS_VERIFY=1
-SET DOCKER_HOST=tcp://192.168.99.100:2376
-SET DOCKER_CERT_PATH=C:\Users\jake\.docker\machine\machines\default
-SET DOCKER_MACHINE_NAME=default
-ECHO ON
-docker build -t jakebladt/build-showman .
-docker push jakebladt/build-showman
-docker launch jakebladt/build-showman
