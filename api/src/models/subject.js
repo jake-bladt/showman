d418f2959c55b0f327e7477bc356e7a548104b0c
Jake Bladt <jake@jakebladt.com>
2016-12-31 11:49:56 -0500

    var->const

--- api/src/models/subject.js
+++ api/src/models/subject.js
@@ -1,12 +1,14 @@
-var mongoose     = require('mongoose');
-var Schema       = mongoose.Schema;
+const mongoose     = require('mongoose');
+const Schema       = mongoose.Schema;
 
-var subjectSchema = new Schema({
+const subjectSchema = new Schema({
   name:         String,
   display_name: String,
   image_count:  Number,
   is_active:    Boolean,
   date_added:   Date
 });
+
+
 
 module.exports = mongoose.model('Subject', subjectSchema);
