746922f16f346d46690353dc2769d64696828c49
Jake Bladt <jake@jakebladt.com>
2017-01-02 21:52:27 -0500

    hyphenation test

--- api/test/models/subject.test.js
+++ api/test/models/subject.test.js
@@ -3,10 +3,14 @@ const subjectModel = require('../../src/models/subject');
 
 describe('#displayNameFromName', () => {
   it('capitalizes the first letter of a simple name', () => {
-    subjectModel.displayNameFromName('jake').should.equal('Jake')
+    subjectModel.displayNameFromName('jake').should.equal('Jake');
   });
 
   it('capitalizes a trailing initial', () => {
-    subjectModel.displayNameFromName('jake.b').should.equal('Jake B')
+    subjectModel.displayNameFromName('jake.b').should.equal('Jake B');
+  });
+
+  it('capitalizes a hyphenated name', () => {
+    subjectModel.displayNameFromName('joe-bob').should.equal('Joe-Bob');
   });
 });
