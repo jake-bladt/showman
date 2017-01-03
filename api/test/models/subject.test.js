3c11cb5837cc71c9e68c9cff7b4b8be0d7dd38be
Jake Bladt <jake@jakebladt.com>
2017-01-02 21:47:13 -0500

    +initial test

--- api/test/models/subject.test.js
+++ api/test/models/subject.test.js
@@ -2,7 +2,11 @@ const should = require('chai').should;
 const subjectModel = require('../../src/models/subject');
 
 describe('#displayNameFromName', () => {
-  it('capitalizes the first letter of a simple name', function() {
+  it('capitalizes the first letter of a simple name', () => {
     subjectModel.displayNameFromName('jake').should.equal('Jake')
+  });
+
+  it('capitalizes a trailing initial', () => {
+    subjectModel.displayNameFromName('jake.b').should.equal('Jake B')
   });
 });
