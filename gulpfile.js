843695616a736ef6ba082442a359221602eafa53
Jake Bladt <jake@jakebladt.com>
2017-01-04 08:46:58 -0500

    +reconcile

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,5 @@
 var gulp = require('gulp');
-var mocha = require('gulp-mocha');
 
-gulp.task('default', function() {
-  return gulp.src(['api/test/*.test.js'], { read: false })
-    .pipe(mocha({
-      reporter: 'spec',
-      globals: {
-        should: require('should')
-      }
-    }));
+gulp.task('reconcile', () => {
+  console.log('reconciling...');
 });
