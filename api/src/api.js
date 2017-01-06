74f673f737923cc5bdded806cff9f12c103fd13b
Jake Bladt <jake@jakebladt.com>
2017-01-06 08:38:57 -0500

    ->=>

--- api/src/api.js
+++ api/src/api.js
@@ -10,7 +10,7 @@ const dburl = process.env.DBURL || secrets.DBUrl;
 const mongoose = require('mongoose');
 mongoose.connect(dburl);
 
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
