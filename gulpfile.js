ada7843e9f4686460c0a27d8b4313cb3e151812f
Jake Bladt <jake@jakebladt.com>
2017-01-05 20:35:02 -0500

    display count only

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,8 @@ const reconciler = require('./scripts/reconciler')
 
 gulp.task('reconcile', () => {
   console.log('reconciling...');
+  console.log('reading from file system.');
   let reader = reconciler.imageLibraryReaderFactory.getReader('fs', path.join(secrets.galleryRoot, 'subjects'));
   let librarySubjects = reader.getSubjects();
-  console.log(librarySubjects.subjects);
+  console.log(`${librarySubjects.subjects.count.toLocaleString()} subjects in file system.`);
 });
