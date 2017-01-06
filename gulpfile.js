70578d6690d3f349453876cdd6e069d3741a06c5
Jake Bladt <jake@jakebladt.com>
2017-01-05 20:16:34 -0500

    append subjects

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
-const secrets = require('./secret');
 const gulp = require('gulp');
+const path = require('path');
+
+const secrets = require('./secret');
 const reconciler = require('./scripts/reconciler')
 
 gulp.task('reconcile', () => {
   console.log('reconciling...');
-  let reader = reconciler.imageLibraryReaderFactory.getReader('fs', secrets.galleryRoot);
+  let reader = reconciler.imageLibraryReaderFactory.getReader('fs', path.join(secrets.galleryRoot, 'subjects'));
   let librarySubjects = reader.getSubjects();
   console.log(librarySubjects.debugInfo);
 });
