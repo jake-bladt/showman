16a956ec8c53b3b7420ad56319ce3a7f461bcf07
Jake Bladt <jake@jakebladt.com>
2017-01-05 20:13:57 -0500

    +return from get...

--- scripts/reconciler.js
+++ scripts/reconciler.js
@@ -8,7 +8,7 @@ var imageLibraryReaderFactory = {
     if('fs' === type) {
       ret.getSubjects = () => {
         let allFilesAndFolders = fs.readdirSync(source);
-        ret.debugInfo = allFilesAndFolders;
+        return { debugInfo: allFilesAndFolders };
       }
     } else { 
       throw 'unrecognized reader type: ' + type 
