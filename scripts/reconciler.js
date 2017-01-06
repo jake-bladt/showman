3664a0aab45f0933dc8d96dab930f27d8b6b20fc
Jake Bladt <jake@jakebladt.com>
2017-01-05 20:27:02 -0500

    +subject

--- scripts/reconciler.js
+++ scripts/reconciler.js
@@ -1,14 +1,29 @@
 var fs = require('fs');
+var path = require('path');
+var subject = require('api/models/subject'); // really need to move this functionality into a utility class
 
 var imageLibraryReaderFactory = {
   getReader: (type, source) => {
-    let subjects = {};
-    let ret = { source, subjects };
+    let ret = { source };
 
     if('fs' === type) {
       ret.getSubjects = () => {
-        let allFilesAndFolders = fs.readdirSync(source);
-        return { debugInfo: allFilesAndFolders };
+        let subjects = {};
+        let subjectFolders = fs.readdirSync(source);
+        subjectFolders.forEach((f) => {
+          let sfPath = path.join(source, f);
+          let sfImages = fs.readdirSync(sfPath);
+          subjects[f] = { 
+            imageCount: sfImages.length,
+            name: f,
+            displayName:  
+          };
+        });
+
+        return {
+          subjects: subjects, 
+          debugInfo: { subjectFolders }
+        };
       }
     } else { 
       throw 'unrecognized reader type: ' + type 
