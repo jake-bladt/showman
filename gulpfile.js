c3871459c390da3b791f065c2ffefbd9e48c30f8
Jake Bladt <jake@jakebladt.com>
2017-01-05 20:38:33 -0500

    +subjects

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,6 @@ gulp.task('reconcile', () => {
   console.log('reconciling...');
   console.log('reading from file system.');
   let reader = reconciler.imageLibraryReaderFactory.getReader('fs', path.join(secrets.galleryRoot, 'subjects'));
-  let librarySubjects = reader.getSubjects();
+  let librarySubjects = reader.getSubjects().subjects;
   console.log(`${Object.keys(librarySubjects).length.toLocaleString()} subjects in file system.`);
 });
