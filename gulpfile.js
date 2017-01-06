8e30304dc191bbc3e2ec0def9b00ba87682300f7
Jake Bladt <jake@jakebladt.com>
2017-01-05 21:04:02 -0500

    +settings object

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,15 @@ const path = require('path');
 const secrets = require('./secret');
 const reconciler = require('./scripts/reconciler')
 
+const settings = {
+  galleryRoot:    process.env.GALLERY_ROOT   || secrets.galleryRoot,
+  gallerySource:  process.env.GALLERY_SOURCE || secrets.gallerySource
+};
+
 gulp.task('reconcile', () => {
   console.log('reconciling...');
   console.log('reading from file system.');
-  let reader = reconciler.imageLibraryReaderFactory.getReader('fs', path.join(secrets.galleryRoot, 'subjects'));
+  let reader = reconciler.imageLibraryReaderFactory.getReader(settings.gallerySource, path.join(settings.galleryRoot, 'subjects'));
   let librarySubjects = reader.getSubjects().subjects;
   console.log(`${Object.keys(librarySubjects).length.toLocaleString()} subjects in file system.`);
 });
