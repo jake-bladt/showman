7c2bd005efd5ffb230f9f18e2e1a3fa75c6081f8
Jake Bladt <jake@jakebladt.com>
2017-04-19 21:05:31 -0400

    +data binding

--- website/scripts/app.js
+++ website/scripts/app.js
@@ -1,5 +1,12 @@
+let viewModel = {
+  subjects: []
+}
+
 function loadSubjects() {
-  $.get('http://localhost:5600/api/subjects', data => console.log(data));
+  $.get('http://localhost:5600/api/subjects', data => {
+    viewModel.subjects = data;
+    ko.applyBindings(viewModel);
+  });
 }
 
 $(document).ready(() => {
